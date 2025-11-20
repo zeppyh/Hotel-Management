c9268ae1d620b37560f8117e743d62a62071fa37
Christian James Caparas <cjcaparas76@gmail.com>
2025-11-21 00:25:58 +0800

    Make room card and home page fully responsive

--- src/page/ErrorPage/ErrorPage.jsx
+++ src/page/ErrorPage/ErrorPage.jsx
@@ -2,9 +2,13 @@ import "./error-page.css";
 
 function ErrorPage() {
   return (
-    <>
-      <div>ErrorPage</div>
-    </>
+    <div className="error-container">
+      <div className="error-content">
+        <h1>404</h1>
+        <h2>Oops! Page Not Found</h2>
+        <p>The page you are looking for does not exist or has been moved.</p>
+      </div>
+    </div>
   );
 }
 
